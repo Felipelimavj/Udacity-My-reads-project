a77e84326d5547e88e987033f1b925ab6617a88c
Felipelimavj <felipe@2arrows.tech>
2023-09-22 13:34:12 +1000

    missing file

---
+++ src/Shelves.js
@@ -0,0 +1,6 @@
+// Shelves.js
+export const shelves = [
+  { key: "currentlyReading", title: "Currently Reading" },
+  { key: "wantToRead", title: "Want to Read" },
+  { key: "read", title: "Read" },
+];
